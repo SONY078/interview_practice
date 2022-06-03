a6c5e0a60471a3cb130e5aab918781172c4b6fd8
SONY078 <71254514+SONY078@users.noreply.github.com>
2022-06-03 20:25:33 +0530

    Add files via upload

---
+++ day5/d5js.js
@@ -0,0 +1,73 @@
+
+function mernStack()
+{
+    let add = document.getElementById("mern");
+    add.innerHTML = "MERN STACK";
+}
+// mernStack();
+
+let g = document.getElementById("day").innerHTML;
+console.log(g);
+
+const d = new Date();
+
+const hr = d.getHours()>12 ?  d.getHours() -12 : d.getHours();
+const min = d.getMinutes();
+const sec = d.getSeconds();
+
+// var time = document.getElementById("time").innerHTML;
+document.write(hr,":"+min,":"+sec);
+
+function changeT()
+{
+    let change = document.getElementById("change");
+    change.innerHTML = "Welcome to elevation academy";
+}
+
+function hideText()
+{
+    let hide = document.getElementById("change");
+    hide.style.display = "none";
+}
+
+
+var arr = [0,1,2,3,0,2,0,4,0];
+
+const findZero = arr.filter(x => x == 0);
+console.log(findZero.length);
+
+var arr = [1,2,3,4,5,6,7,8,9,10];
+let Ecount =0,Ocount=0;
+for(let i=0;i<arr.length;i++)
+{
+    if(arr[i]%2==0)
+    {
+        Ecount++;
+    }
+    else
+    {
+        Ocount++;
+    }
+}
+console.log(Ecount);
+console.log(Ocount);
+
+// var str = ["hello"];
+
+
+
+
+
+let c1 = 0;
+
+// v = [ "a","e","i","o","u"];
+
+let findVowel = string => [...string].filter(c1 => 'aeiou'.includes(c1.toLowerCase())).length;
+
+console.log("vowel count of 'hello':",findVowel('hello'));
+console.log("vowel count of 'how are you':",findVowel('how are you'));
+console.log("vowel count of 'fine':",findVowel('fine'));
+
+
+
+
