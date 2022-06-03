0a278736526d33fade5d16d9411a02b4d2a3ce6a
SONY078 <71254514+SONY078@users.noreply.github.com>
2022-06-03 20:33:16 +0530

    Add files via upload

--- day5/d5js.js
+++ day5/d5js.js
@@ -68,6 +68,65 @@ console.log("vowel count of 'hello':",findVowel('hello'));
 console.log("vowel count of 'how are you':",findVowel('how are you'));
 console.log("vowel count of 'fine':",findVowel('fine'));
 
+//pass by value
+function Passbyvalue(a, b) 
+{
+    let tmp;
+    tmp = b;
+    b = a;
+    a = tmp;
+    console.log(`Inside Pass by value 
+        function -> a = ${a} b = ${b}`);
+}
+  
+let a = 1;
+let b = 2;
+console.log(`Before calling Pass by value 
+        Function -> a = ${a} b = ${b}`);
+  
+Passbyvalue(a, b);
+  
+console.log(`After calling Pass by value 
+       Function -> a =${a} b = ${b}`);
 
 
+//op:
+/*Before calling Pass by value 
+        Function -> a = 1 b = 2
+Inside Pass by value 
+        function -> a = 2 b = 1
+After calling Pass by value 
+       Function -> a =1 b = 2*/
 
+//pass by reference
+function PassbyReference(obj) 
+{
+    let tmp = obj.a;
+    obj.a = obj.b;
+    obj.b = tmp;
+  
+    console.log(`Inside Pass By Reference 
+        Function -> a = ${obj.a} b = ${obj.b}`);
+}
+  
+let obj = 
+{
+    a: 10,
+    b: 20
+  
+}
+console.log(`Before calling Pass By Reference 
+    Function -> a = ${obj.a} b = ${obj.b}`);
+  
+PassbyReference(obj)
+  
+console.log(`After calling Pass By Reference 
+    Function -> a = ${obj.a} b = ${obj.b}`);
+
+    //op
+/* Before calling Pass By Reference 
+    Function -> a = 10 b = 20
+ Inside Pass By Reference 
+        Function -> a = 20 b = 10
+ After calling Pass By Reference 
+    Function -> a = 20 b = 10*/
